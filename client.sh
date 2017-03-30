d244e6599a4fab7ea746f5bbfd99464dc825638a
李正强 <lizhengqiang@mouge.wang>
2017-03-30 20:42:05 +0800

    Update client.sh

--- client.sh
+++ client.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-echo {"School":{"ID":"$SCHOOL_ID","Name":"$SCHOOL_ID"},"ClientID":"$CLIENT_ID"} > ./config
+echo '{"School":{"ID":"'$SCHOOL_ID'","Name":"'$SCHOOL_ID'"},"ClientID":"'$CLIENT_ID'"}' > ./config
 ./mxz_3.0.4.1_386.run
